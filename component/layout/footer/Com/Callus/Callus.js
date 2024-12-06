2a444e4c20ad7648e7c6721e971fcdfa5a5252a7
arjun <saharjun031@gmail.com>
2024-12-06 17:46:18 +0545

    udpdatedggghhhhjj

--- component/layout/footer/Com/Callus/Callus.js
+++ component/layout/footer/Com/Callus/Callus.js
@@ -7,7 +7,7 @@ const Callus = () => {
             <div>Call us</div>
             {/* 'tel:' protocol used for phone call */}
             <a className={CssStyle.Number} href='tel:9709475100'>
-                <span>9709574100</span>
+                <span>9709475100</span>
             </a>
         </div>
     );
